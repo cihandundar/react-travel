0ccd1213481c5b144dbda12427170840f8823b4d
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-06-29 20:17:48 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Blog from "./Blog";
+import Contacts from "./Contacs";
 import Header from "./Header";
-import Hero from "./Hero";
-import Section from "./Section";
 import Footer from "./Footer";
 
 function App() {
   return (
     <>
-      <Header />
-      <Hero />
-      <Section />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/blog" component={Blog} exact />
+          <Route path="/contacts" component={Contacts} exact />
+        </Switch>
+      </Router>
       <Footer />
     </>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
+  const [isMobile, setIsMobile] = useState(false);
   return (
     <header className="header">
       <div className="header__logo">
         <h2>Travel.co</h2>
       </div>
       <nav className="header__nav">
-        <ul className="header__nav__list">
-          <li className="header__nav__link">Home</li>
-          <li className="header__nav__link">Blog</li>
-          <li className="header__nav__link">Contacts</li>
-          <li className="header__nav__link">Page 1</li>
-          <li className="header__nav__link">Page 2</li>
+        <ul
+          className={
+            isMobile ? "header__nav__list__mobile" : "header__nav__list"
+          }
+          onClick={() => setIsMobile(false)}
+        >
+          <li className="header__nav__link">
+            <Link to="/home">Home</Link>
+          </li>
+          <li className="header__nav__link">
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li className="header__nav__link">
+            <Link to="/contacts">Contacts</Link>
+          </li>
+          <li className="header__nav__link">
+            <Link to="/page1">Page 1</Link>
+          </li>
+          <li className="header__nav__link">
+            <Link to="/page2">Page 2</Link>
+          </li>
         </ul>
       </nav>
+      <button className="header__mobile" onClick={() => setIsMobile(!isMobile)}>
+        {isMobile ? (
+          <i className="fas fa-times"></i>
+        ) : (
+          <i className="fas fa-bars"></i>
+        )}
+      </button>
     </header>
   );
 }
